b15efd9de3c0a84ef8f352584a156568ca23f83d
tommyerr <twilliams241@boyslatin.net>
2024-11-18 18:55:50 +0000

    added async nov 18

---
+++ unit3_javasrcipt/asyncnov15.js
@@ -0,0 +1,28 @@
+// 1. 
+
+var username = "nameuser"
+var password = "wordpass"
+
+var securitysystemusername
+var securitypsystempassword
+
+console.log(username == securitysystemusername)
+// 2. 
+
+var EcgOrderRequest = 3
+var ecgShipmentRecieved = 20
+
+// 3.
+
+var messagetosend = 'can you see this'
+var messagetorec = messagetosend
+
+// 4. 
+
+// = 'single equal'
+
+// && 'double ampersand'
+
+// || 'double vertical slash'
+
+// == 'double equal'
