609c634831351f04380df39725781f939d80978e
tommyerr <twilliams241@boyslatin.net>
2024-11-19 20:13:36 +0000

    added function practice folder and added function js file with home html file

---
+++ unit3_javasrcipt/functionpractice/function.js
@@ -0,0 +1,28 @@
+// What is a Function
+'A function is a set instructions that performs a specific task or job'
+
+// Functions come in 2 variations:
+// built in functions- there are instructions that are pre written
+// into the coding language all we need to do is pass in data
+
+
+// examples
+var classname= 'coding'
+console.log(classname)
+
+
+
+// user defined functions- there are custom instructions written by
+// engineer to make code that performs specific task
+
+// how do we write a user-defined function?
+
+function examplefunction(){
+        console.log('the function script is connected')
+}
+// in order to have functions work we need to call it 
+// the above code is the function defintion meaning its 
+// only telling the computer instructions not to actually run it
+
+// we access or call a function by writing its name
+examplefunction()
