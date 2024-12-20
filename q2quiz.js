da2108683727ff08b9910335071096e83151c94c
tommyerr <twilliams241@boyslatin.net>
2024-12-20 20:19:18 +0000

    did q2 quiz didnt complete

---
+++ q2quiz.js
@@ -0,0 +1,24 @@
+"1. A function is written code to perform a certain thing in your code."
+
+"2. A Parameter is the name given to the function and a Argument is the values in the function"
+
+"3. An if statement is telling the code what function should be used if a certain value or condition is met in the code. An else statement is telling the code what function should be used when the value isnt met"
+
+"4.integer data types are used to hold values in your code."
+
+"5. Boolean data types are used to show if the value is true or false"
+
+"6. The operator to use is the if/else function because it can tell if the age meets the required condition or not"
+
+"7. The boolean operator should be used because it could tell the code if the entered passcode is true or false to enter the building."
+
+"8." 
+let password;
+
+if (pass < 10){
+    password = "password too long";
+}
+else { password = "usable";
+    
+}
+
