fd92840643bb5ca0a4ede6594ea874023ce2f8ee
tommyerr <twilliams241@boyslatin.net>
2025-01-10 20:07:30 +0000

    async jan 10

---
+++ unit 4/asyncactivityjan_15.js
@@ -0,0 +1,14 @@
+frozenfood=['fish','meat','fruit','pizza','sassuage']
+internationalfood=['samosa','mochi','pavlova','ceviche','poutine']
+produce=['grapes','peppers','mushrooms','bananas','tomato']
+
+
+
+let numbers = [1,30,39,50,293,100]
+var numberline = [numbers]
+
+console.log(numberline)
+
+let result = multiplyAtIndex4(numbers, 4);
+
+console.log(result)
---
+++ unit 4/javascriptloops.js
@@ -0,0 +1,50 @@
+function collegeTitileByYear(year){
+
+if(year=1){
+    console.log("you are a freshman.");
+}
+
+
+else if (year == 2){
+    console.log("you are a sophmore");
+}
+
+else if(year== 3){
+    console.log("you are a junior")
+}
+
+else if(year== 4)("you are a senior")
+else if(year== 5 || year== 6){
+    console.log("you are in a masters program")
+}
+
+else{
+console.log('error data not accepted')
+}
+}
+
+collegeTitileByYear(5);
+
+// a js list is a data type that is used to group
+//and organize other data types in one variable
+
+// we use square brackets to designate something 
+// as a list
+
+
+grocerylist = ['water','apples','bread']
+var randomdata = [grocerylist]
+
+console.log(randomdata)
+
+coding1s=['black cw','green','white cw']
+coding2s=['red cw','blue cw']
+coding3s=[]
+
+console.log("these are the shows in stock")
+console.log(coding1s,coding2s,coding3s)
+
+delimeats=['turkey','ham','roastbeef']
+cleaningsupplies=['dish soap','paper towels','soap']
+snakcs=['chips','candy','soda','juice']
+
