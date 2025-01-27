d5d8090d7c536c2e7d044fe5b57e530ce3dbe001
tommyerr <twilliams241@boyslatin.net>
2025-01-27 18:57:25 +0000

    added async jan24

---
+++ unit 4/asyncactivity24.js
@@ -0,0 +1,18 @@
+var numberlist = [1,23,56,3,56,3,20,200]
+
+//create a function that uses a while loop to print/ console.log
+//the list but in reverse order
+
+reversenumberlist = numberlist.reverse()
+console.log(reversenumberlist)
+
+function reversenumbers(){
+
+    var i = 0
+    while( i > 0){
+        console.log(numberlist(i))
+        i -=1
+    } 
+} 
+
+reversenumbers()
---
+++ unit 4/whileloops.js
@@ -0,0 +1,21 @@
+//loops = a loop is a special construct that repeats
+//a set of code instructions over and over under
+//specific conditions.
+
+//while loops = this is a special type of loop
+//where so long as a condition is true it will
+//repeat the conde instructions over and over.
+
+
+x = 2  //this is an ex of an assignment
+//operator - giving a variable a value
+
+
+//= assignment operator
+//== comparison operator (same as)
+//|= comparison operator (not the same as)
+
+
+while (x == 2){
+    console.log('this message will repeat forever')
+}
